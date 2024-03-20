6d920d4b2b13d714eb89908eaf9bedc7e5d9376f
Zahrasapitri <zsapitri65@gmail.com>
2024-03-20 15:09:56 +0700

    upload pertemuan5

---
+++ pertemuan5/data_kenadraan.php
@@ -0,0 +1,13 @@
+<?php
+    require_once 'class_kendaraan.php';
+
+    $motor = new Motor('Motor', "Bensin", 2);
+    $submarine = new Submarine('Submarine', 'Solar', 1000);
+
+    echo "Info Motor:<br>";
+    $motor->getInfoMotor();
+    echo "<br>";
+
+    echo "Info Submarine:<br>";
+    $submarine->getInfoSubmarine();
+?>
